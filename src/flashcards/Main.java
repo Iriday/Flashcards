ce92ae7c5b9d21ff983812f86854ccf2dfdc8972
Igor <Iriday@mail.ru>
2019-12-05 15:12:12 +0200

    Initial commit

---
+++ src/flashcards/Main.java
@@ -0,0 +1,8 @@
+package flashcards;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card:\n" + "black");
+        System.out.println("Definition:\n" + "white");
+    }
+}
