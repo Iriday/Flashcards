249ef45c7a01fb4d96a546e937f12a2d8db8d8ed
Igor <Iriday@mail.ru>
2019-12-05 15:50:51 +0200

    Some small additions

--- src/flashcards/Main.java
+++ src/flashcards/Main.java
@@ -1,8 +1,19 @@
 package flashcards;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Card:\n" + "black");
-        System.out.println("Definition:\n" + "white");
+  /*      System.out.println("Card:\n" + "black");
+        System.out.println("Definition:\n" + "white");*/
+
+        Scanner scn = new Scanner(System.in);
+
+        String term = scn.nextLine();
+        String definition = scn.nextLine();
+        String answer = scn.nextLine();
+
+        String[] card = {term, definition};
+        System.out.println(card[1].equals(answer) ? "Your answer is right!" : "Your answer is wrong...");
     }
 }
